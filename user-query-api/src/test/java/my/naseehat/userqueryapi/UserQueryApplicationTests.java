0d8bb0978027bc05c1b797ef5032fedbbd7b4747
Mohammad Harith <15016508+harixth@users.noreply.github.com>
2022-06-14 17:01:01 +0800

    initial

---
+++ user-query-api/src/test/java/my/naseehat/userqueryapi/UserQueryApplicationTests.java
@@ -0,0 +1,9 @@
+package my.naseehat.userqueryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserQueryApplicationTests {
+
+}
